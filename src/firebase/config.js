4989634f922712d8339680e69a950841d9350c0a
Edgar Pinheiro <edgar.melo.prestador@prf.gov.br>
2023-11-16 16:20:33 -0300

    Login - Registrando usuário no sistema (firebase) Correção nas configurações do firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA3PPP4cR1_ROL1RO2PykgbFAxQcUnznAM",

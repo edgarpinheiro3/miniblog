3a989368aa60805dd5ea8f914fdcaff161cdd4a9
Edgar Pinheiro <edgar.melo.prestador@prf.gov.br>
2023-11-20 16:33:43 -0300

    Estruturando o dashboard HTML e CSS

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,31 @@
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+//hooks
+import { useAuthValue } from "../../context/AuthContext"; //pega as informações do usuário
+import { useFetchDocuments } from "../../hooks/useFetchDocuments"; //pega os posts do usuário
+
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  //pegar posts do usuário
+  const posts = [];
+
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className={styles.dashboard}>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts!</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div>Tem posts!</div>
+      )}
     </div>
   );
 };
